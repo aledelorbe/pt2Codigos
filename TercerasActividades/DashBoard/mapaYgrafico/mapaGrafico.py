816120af45d1cfd73f8ba38bb23fba9e60508979
aledelorbe <mgranadosb1800@alumno.ipn.mx>
2024-05-22 16:16:22 -0600

    se agrego graficos por defecto y se arreglo titulo de grafico

--- TercerasActividades/DashBoard/mapaYgrafico/mapaGrafico.py
+++ TercerasActividades/DashBoard/mapaYgrafico/mapaGrafico.py
@@ -98,6 +98,95 @@ def aplicarEstilosMapa(fig, tituloX):
     return fig
 
 
+def generadorGraficos(parametroVerda, numeroId, diccColores):
+    # Consultar los datos que permiten la creacion de los graficos referentes al parametro seleccionado
+    estado, numCluster, etiquetasParam, cantidadesParam, porcentajesParam = db.consultaBarras(parametroVerda, numeroId)
+
+    # Consultar los datos que permiten la creacion de los graficos referentes a los tipos de cáncer
+    estado, numCluster, etiquetasCancer, cantidadesCancer, porcentajesCancer = db.consultaBarras('Tipo de Cancer', numeroId)
+
+    # Crear la grafica de barras para el parametro seleccionado (forma cruda)
+    dataBarraParam = [go.Bar(x=etiquetasParam, 
+                        y=cantidadesParam,
+                        marker=dict(
+                            color=diccColores[str(numCluster)],  
+                            line=dict(color='black')  
+                        ))] 
+    estilosFiguraParam = go.Layout(
+        title=f"""         Cantidad de personas con cáncer en los años 2010 a 2019 por cada {parametroVerda} en el 
+                <br>                                 estado de {estado}""",
+        xaxis=dict(title=parametroVerda),
+        yaxis=dict(title="Cantidad"),
+        font=dict(
+            family='Verdana',
+            size=16,
+            color='black'
+        )
+    )
+    figuraParam = go.Figure(data=dataBarraParam, layout=estilosFiguraParam)
+
+    # Crear la grafica de barras para los tipos de cancer (forma cruda)
+    dataBarraCancer = [go.Bar(x=etiquetasCancer, 
+                        y=cantidadesCancer,
+                        marker=dict(
+                            color=diccColores[str(numCluster)],  
+                            line=dict(color='black')  
+                        ))] 
+    estilosFiguraCancer = go.Layout(
+        title=f"""Cantidad de personas con cáncer en los años 2010 a 2019 por cada Tipo de Cáncer en el 
+                <br>                                 estado de {estado}""",
+        xaxis=dict(title='Tipos de Cáncer'),
+        yaxis=dict(title="Cantidad"),
+        font=dict(
+            family='Verdana',
+            size=16,
+            color='black'
+        )
+    )
+    figuraCancer = go.Figure(data=dataBarraCancer, layout=estilosFiguraCancer)
+
+    # Crear la grafica de barras para el parametro seleccionado (forma porcentual)
+    dataBarraParam_0_1 = [go.Bar(x=etiquetasParam, 
+                        y=porcentajesParam,
+                        marker=dict(
+                            color=diccColores[str(numCluster)],  
+                            line=dict(color='black')  
+                        ))] 
+    estilosFiguraParam_0_1 = go.Layout(
+        title=f"""Porcentaje de la cantidad de personas con cáncer en los años 2010 a 2019 por cada {parametroVerda} 
+                <br>                                 en el estado de {estado}""",
+        xaxis=dict(title=parametroVerda),
+        yaxis=dict(title="Porcentaje"),
+        font=dict(
+            family='Verdana',
+            size=16,
+            color='black'
+        )
+    )
+    figuraParam_0_1 = go.Figure(data=dataBarraParam_0_1, layout=estilosFiguraParam_0_1)
+
+    # Crear la grafica de barras para los tipo de cancer (forma porcentual)
+    dataBarraCancer_0_1 = [go.Bar(x=etiquetasCancer, 
+                        y=porcentajesCancer,
+                        marker=dict(
+                            color=diccColores[str(numCluster)],  
+                            line=dict(color='black')  
+                        ))] 
+    estilosFiguraCancer_0_1 = go.Layout(
+        title=f"""Porcentaje de la cantidad de personas con cáncer en los años 2010 a 2019 por cada Tipo de Cáncer 
+                <br>                                 en el estado de {estado}""",
+        xaxis=dict(title='Tipo de Cáncer'),
+        yaxis=dict(title="Porcentaje"),
+        font=dict(
+            family='Verdana',
+            size=16,
+            color='black'
+        )
+    )
+    figuraCancer_0_1 = go.Figure(data=dataBarraCancer_0_1, layout=estilosFiguraCancer_0_1)
+
+    return figuraParam, figuraCancer, figuraParam_0_1, figuraCancer_0_1
+
 
 
 app = dash.Dash()
@@ -105,10 +194,13 @@ app = dash.Dash()
 
 # MAPA QUE SE MOSTRARA POR DEFECTO
 # Genera la figura
-fig = go.Figure(data=generadorDeMapas(db.extraerClustersEstadoEducacion(), funcAux.coloresEducacion))
-
+mapaDefecto = go.Figure(data=generadorDeMapas(db.extraerClustersEstadoEducacion(), funcAux.coloresEducacion))
 # Aplicar estilos
-fig = aplicarEstilosMapa(fig, 'Estados de México feat Nivel Educativo y Cáncer')
+mapaDefecto = aplicarEstilosMapa(mapaDefecto, 'Estados de México feat Nivel Educativo y Cáncer')
+
+# GRAFICOS QUE SE MOSTRARAN POR DEFECTO
+# Graficos crudos y porcentuales
+figuraParam, figuraCancer, figuraParam_0_1, figuraCancer_0_1 = generadorGraficos('Nivel Educativo', 1, funcAux.coloresEducacion)
 
 
 app.layout = html.Div([
@@ -118,27 +210,32 @@ app.layout = html.Div([
                  value='Nivel Educativo y Cáncer'),
     dcc.Graph(
         id='mapa',
-        figure=fig
+        figure=mapaDefecto
     ),
     html.Div([
         html.Pre(id='informacionClick')
     ]),
+    # GRAFICOS CRUDOS
     dcc.Graph(
         id='barraParametro',
-        # figure=fig
+        figure=figuraParam
     ),
-    # dcc.Graph(
-    #     id='barraCancer',
-    #     # figure=fig
-    # ),
+    dcc.Graph(
+        id='barraCancer',
+        figure=figuraCancer
+    ),
+    # GRAFICOS PORCENTUALES
+    html.H2(
+        'Representacion porcentual de los datos'
+    ),
     dcc.Graph(
         id='barraParametro_0_1',
-        # figure=fig
+        figure=figuraParam_0_1
     ),
-    # dcc.Graph(
-    #     id='barraCancer_0_1',
-    #     # figure=fig
-    # ),
+    dcc.Graph(
+        id='barraCancer_0_1',
+        figure=figuraCancer_0_1
+    ),
 ])
 
 
@@ -185,7 +282,7 @@ def extractorInformacionClick(informacion, parametro):
 # ACTUALIZADOR DE BARRA
 # Cambia las graficas de barras dependientemenete del estado que se seleccione en el mapa
 @app.callback(
-    [Output('barraParametro', 'figure'), Output('barraParametro_0_1', 'figure')],
+    [Output('barraParametro', 'figure'), Output('barraCancer', 'figure'), Output('barraParametro_0_1', 'figure'), Output('barraCancer_0_1', 'figure')],
     [Input('parametro', 'value'), Input('mapa', 'clickData')]
 )
 def actualizarBarra(parametro, informacion):
@@ -203,49 +300,8 @@ def actualizarBarra(parametro, informacion):
     numeroId = informacion['points'][0]['curveNumber'] + 1 # El +1 porque el primer estado es 0 pero en la db es 1
     numeroId = funcAux.corregirIndice(numeroId) # Corregir indice
 
-    # Consultar los datos que permiten la creacion del grafico
-    estado, numCluster, etiquetas, cantidades, porcentajes = db.consultaBarras(parametroVerda, numeroId)
-    # print(estado, numCluster, etiquetas, cantidades)
+    return generadorGraficos(parametroVerda, numeroId, diccColores)
 
-    # Crear la grafica de barras para el parametro seleccionado (forma cruda)
-    dataBarraParam = [go.Bar(x=etiquetas, 
-                        y=cantidades,
-                        marker=dict(
-                            color=diccColores[str(numCluster)],  
-                            line=dict(color='black')  
-                        ))] 
-    estilosFiguraParam = go.Layout(
-        title=f"Cantidad de personas con cáncer en los años 2010 a 2019 por cada {parametroVerda} en el \n \testado de {estado}",
-        xaxis=dict(title=parametroVerda),
-        yaxis=dict(title="Cantidad"),
-        font=dict(
-            family='Verdana',
-            size=16,
-            color='black'
-        )
-    )
-    figuraParam = go.Figure(data=dataBarraParam, layout=estilosFiguraParam)
-
-    # Crear la grafica de barras para el parametro seleccionado (forma porcentual)
-    dataBarraParam_0_1 = [go.Bar(x=etiquetas, 
-                        y=porcentajes,
-                        marker=dict(
-                            color=diccColores[str(numCluster)],  
-                            line=dict(color='black')  
-                        ))] 
-    estilosFiguraParam_0_1 = go.Layout(
-        title=f"Porcentaje de la cantidad de personas con cáncer en los años 2010 a 2019 por cada {parametroVerda} \n \ten el estado de {estado}",
-        xaxis=dict(title=parametroVerda),
-        yaxis=dict(title="Porcentaje"),
-        font=dict(
-            family='Verdana',
-            size=16,
-            color='black'
-        )
-    )
-    figuraParam_0_1 = go.Figure(data=dataBarraParam_0_1, layout=estilosFiguraParam_0_1)
-
-    return figuraParam, figuraParam_0_1
 
 if __name__ == '__main__':
     app.run_server()
