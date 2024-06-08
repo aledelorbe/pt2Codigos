ffc042e2fc270becbd26a41d3e268ca13bac311f
aledelorbe <mgranadosb1800@alumno.ipn.mx>
2024-06-07 18:17:04 -0600

    se agregaron los template que pidio bella

---
+++ TercerasActividades/Servidor/models/educacionCancer.py
@@ -0,0 +1,24 @@
+def datosEducacionCancer():
+    eduGrupo0 = ['Guanajuato', 'Hidalgo', 'Morelos', 'Puebla', 'San Luis Potosí', 'Tlaxcala']
+    eduGrupo1 = ['Baja California', 
+                 'Baja California Sur', 
+                 'Chihuahua', 
+                 'Coahuila de Zaragoza', 
+                 'Nuevo León', 
+                 'Sonora', 
+                 'Tamaulipas']
+    eduGrupo2 = ['Campeche', 
+                 'Guerrero', 
+                 'Tabasco', 
+                 'Veracruz de Ignacio de la Llave', 
+                 'Yucatán']
+    eduGrupo3 = ['Distrito Federal']
+    eduGrupo4 = ['Aguascalientes', 'Durango', 'Jalisco']
+    eduGrupo5 = ['México', 'Querétaro']
+    eduGrupo6 = ['Chiapas', 'Oaxaca']
+    eduGrupo7 = ['Quintana Roo']
+    eduGrupo8 = ['Michoacán de Ocampo', 'Nayarit', 'Zacatecas']
+    eduGrupo9 = ['Colima', 'Sinaloa']
+
+    eduGrupos = [eduGrupo0, eduGrupo1, eduGrupo2, eduGrupo3, eduGrupo4, eduGrupo5, eduGrupo6, eduGrupo7, eduGrupo8, eduGrupo9]
+    return eduGrupos
---
+++ TercerasActividades/Servidor/models/empleoCancer.py
@@ -0,0 +1,17 @@
+def datosEmpleoCancer():
+    empGrupo0 = ['Hidalgo', 'San Luis Potosí']
+    empGrupo1 = ['Chihuahua', 'Coahuila de Zaragoza', 'Colima', 'Durango', 'Sonora', 'Tamaulipas']
+    empGrupo2 = ['Guerrero', 'Tabasco', 'Veracruz de Ignacio de la Llave']
+    empGrupo3 = ['México', 'Querétaro']
+    empGrupo4 = ['Quintana Roo']
+    empGrupo5 = ['Aguascalientes', 'Guanajuato', 'Jalisco', 'Morelos']
+    empGrupo6 = ['Baja California', 'Baja California Sur', 'Nuevo León']
+    empGrupo7 = ['Chiapas', 'Oaxaca']
+    empGrupo8 = ['Campeche', 'Yucatán']
+    empGrupo9 = ['Distrito Federal']
+    empGrupo10 = ['Michoacán de Ocampo', 'Nayarit', 'Zacatecas']
+    empGrupo11 = ['Puebla', 'Tlaxcala']
+    empGrupo12 = ['Sinaloa']
+
+    empGrupos = [empGrupo0, empGrupo1, empGrupo2, empGrupo3, empGrupo4, empGrupo5, empGrupo6, empGrupo7, empGrupo8, empGrupo9, empGrupo10, empGrupo11, empGrupo12]
+    return empGrupos
